a343961a1948526755193cfd176dfba3a485250c
Timothy Winters <t.winters099@gmail.com>
2022-08-21 00:10:53 -0400

    RequestBody can be a

--- src/Attributes/RequestBody.php
+++ src/Attributes/RequestBody.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace OpenApiGenerator\Attributes;
 
 use Attribute;
+use OpenApiGenerator\Types\ItemsType;
 use OpenApiGenerator\Types\RequestBodyType;
 use JsonSerializable;
+use OpenApiGenerator\Types\SchemaType;
 
 /**
  * The entire request body where you can specify the return type and a schema
@@ -17,9 +19,21 @@ class RequestBody implements JsonSerializable
     private ?Schema $schema = null;
 
     public function __construct(
-        private ?string $type = null
+        private ?string $type = null,
+        private ?string $schemaType = SchemaType::OBJECT,
+        private ?string $ref = null
     ) {
         $this->type ??= RequestBodyType::JSON;
+
+        if ($ref) {
+            $this->schema = new Schema($schemaType);
+
+            if ($schemaType === SchemaType::OBJECT) {
+                $this->schema->addProperty(new RefProperty($ref));
+            } elseif ($schemaType === SchemaType::ARRAY) {
+                $this->schema->addProperty(new PropertyItems(ItemsType::REF, $ref));
+            }
+        }
     }
 
     public function setSchema(Schema $schema): void
